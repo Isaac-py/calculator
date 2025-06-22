653f1131ca7078df4574d566ad5d03d66596fad1
Aziret-Ali Ismailov <ismailovisaac@gmail.com>
2025-06-22 09:53:09 -0400

    Clean up basic calculator operations

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const add = function(a,b) {
-	return a+b;
+let total;
+let num1;
+let num2;
+
+const operate = function(num1, num2, operator) {
+    if (operator === '+') {
+        total = add(num1, num2);
+    } else if (operator === '-') {
+        total = subtract(num1, num2);
+    } else if (operator === '/') {
+        total = divide(num1, num2);
+    } else if (operator === '*') {
+        total = multiply(num1, num2);
+    }
+    return total;
+}
+const add = function(num1,num2) {
+	return num1 + num2;;
 };
 
-const subtract = function(a,b) {
-	return a-b;
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const multiply = function(a,b) {
-    return a*b;
+const multiply = function(num1,num2) {
+	return num1*num2;
 };
 
-const divide = function(a, b) {
-    return a/b;
+const divide = function(num1, num2) {
+	return num1/num2;
 }
 
